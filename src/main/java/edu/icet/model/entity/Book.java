6bd531d58731bed586c72ab07be28db74d73ad77
UdaraDewshan <dewudara@gmail.com>
2026-01-16 01:23:26 +0530

    Add Booking Entity

---
+++ src/main/java/edu/icet/model/entity/Book.java
@@ -0,0 +1,18 @@
+package edu.icet.model.entity;
+
+import java.util.Date;
+
+public class Book {
+    private String bookingId;
+
+    private String userId;
+    private String carId;
+    private String customerId;
+    private String driverId;
+
+    private Date startDate;
+    private Date endDate;
+    private boolean withDriver;
+    private Double totalPrice;
+
+}
