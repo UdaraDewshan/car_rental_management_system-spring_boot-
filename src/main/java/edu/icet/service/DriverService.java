b2596d1481febf5d4c442b294e6b5816b37a143f
UdaraDewshan <dewudara@gmail.com>
2026-01-28 23:57:25 +0530

    Add Driver Add Api

--- src/main/java/edu/icet/service/DriverService.java
+++ src/main/java/edu/icet/service/DriverService.java
@@ -2,6 +2,7 @@ package edu.icet.service;
 
 import edu.icet.model.dto.DriverDTO;
 import edu.icet.model.entity.Driver;
+import edu.icet.repository.DriverRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,11 @@ import org.springframework.stereotype.Service;
 public class DriverService {
 
     final ModelMapper modelMapper;
+    final DriverRepository driverRepository;
 
     public String addDriver(DriverDTO driverDTO) {
         Driver driver = modelMapper.map(driverDTO, Driver.class);
-        return null;
+        driverRepository.save(driver);
+        return "Driver Added successfully";
     }
 }
