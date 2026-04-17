64b66932e52fddcacdf11f0bfe4cde3e633ea77d
UdaraDewshan <dewudara@gmail.com>
2026-04-18 00:11:19 +0530

    Add AuthenticationController and AuthenticationService for user authentication

---
+++ src/main/java/edu/icet/service/AuthenticationService.java
@@ -0,0 +1,16 @@
+package edu.icet.service;
+
+import edu.icet.model.dto.AuthRequest;
+import edu.icet.model.dto.AuthResponse;
+import edu.icet.model.dto.RegisterRequest;
+import org.jspecify.annotations.Nullable;
+
+public class AuthenticationService {
+    public @Nullable AuthResponse register(RegisterRequest request) {
+        return null;
+    }
+
+    public @Nullable AuthResponse authenticate(AuthRequest request) {
+        return null;
+    }
+}
