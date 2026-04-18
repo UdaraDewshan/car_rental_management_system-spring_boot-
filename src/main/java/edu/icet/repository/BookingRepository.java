5228b82ec1ee55b722e153589a55f88724fd600c
UdaraDewshan <dewudara@gmail.com>
2026-04-18 21:19:04 +0530

    Add BookingRepository for data access layer in booking management

---
+++ src/main/java/edu/icet/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package edu.icet.repository;
+
+import edu.icet.model.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, String> {
+}
