e9462d500f26a2ce9e50ca7729c21feef7a2da33
UdaraDewshan <dewudara@gmail.com>
2026-04-23 19:13:07 +0530

    Add UserController and UserService to handle user retrieval

---
+++ src/main/java/edu/icet/service/UserService.java
@@ -0,0 +1,15 @@
+package edu.icet.service;
+
+import edu.icet.model.dto.UserDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    public List<UserDTO> getAllUsers() {
+        return null;
+    }
+}
